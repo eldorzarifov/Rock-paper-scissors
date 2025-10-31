f7335742bd0cb78bb0c86f4ae8f7dc447d05682f
eldorzarifov <eldorbekzarifov5@gmail.com>
2025-10-31 20:28:55 +0500

    V1.1 Computer choice modify Human choice finish Simplified Computer choice and added human choice part

--- script.js
+++ script.js
@@ -5,16 +5,33 @@
 function getComputerChoice(){
     choice = Math.floor(Math.random()*100)
     if(choice <= 33){
-        choice = "rock";
-        return choice;
+        return "rock";
     }   
     else if(choice <= 66){
-        choice = "paper"
-        return choice;
+        return "paper";
     }
     else{
-        choice = "scissors";
-        return choice;
+        return "scissors";
     };
 }
 console.log(getComputerChoice())
+// New function for human choice => getHumanChoice
+function getHumanChoice(){
+    let option = prompt("Rock, Paper, Scissors");
+    if(option.toLowerCase() === "rock"){
+        console.log('rock');
+        return option;
+    }
+    else if(option.toLowerCase() === "paper"){
+        console.log('paper');
+        return option;
+    }
+    else if(option.toLowerCase() === "scissors"){
+        console.log('scissors');
+        return option;
+    }
+    else{
+        console.log('Please choose among Rock, Paper, Scissors')
+    }
+}
+getHumanChoice()
