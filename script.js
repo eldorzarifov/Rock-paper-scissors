28d355a371df2fdc2f912e97c3aeee6de9fdd312
eldorzarifov <eldorbekzarifov5@gmail.com>
2025-11-22 23:11:25 +0500

    V1.4 Add UI Modify logic Added 3 buttons, added event listeners to them. Now it uses button to get humanchoice instead of entering through prompts

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 computer = 0
 human = 0
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors")
 // Function that gets value by math random => getComputerChoice
     // First conditional checks value if it's between 0 and 33
     // Second conditional checks value if it's between 34 and 66
@@ -17,71 +20,27 @@ function getComputerChoice(){
     };
 }
 // console.log(getComputerChoice())
-// New function for human choice => getHumanChoice
-function getHumanChoice(){
-    let option = prompt("Rock, Paper, Scissors");
-    let fixed = option.toLowerCase().trim();
-    if(fixed === "rock"){
-        console.log('rock');
-        return fixed;
-    }
-    else if(fixed === "paper"){
-        console.log('paper');
-        return fixed;
-    }
-    else if(fixed === "scissors"){
-        console.log('scissors');
-        return fixed;
-    }
-    else{
-        console.log('Please choose among Rock, Paper, Scissors');
-    };
-}
-// getHumanChoice()
 
 // //Function for playing a round, it takes choices and finds the winner and adds 1 to their score
 function playRound(humanChoice, computerChoice){
-    if(humanChoice === computerChoice){
-        console.log("draw");
+    if (humanChoice === computerChoice) {
+        console.log("Draw");
         return;
     }
-    else if(humanChoice === "scissors" && computerChoice === "paper"){
-        human +=1;
-        console.log("Scissors beat paper, You win!");
-    }
-    else if(humanChoice === "rock" && computerChoice === "scissors"){
-        human +=1;
-        console.log("Rock beats Scissors, You win!");
-    }
-    else if(humanChoice === "paper" && computerChoice === "rock"){
-        human +=1;
-        console.log("Paper beats Rock, You win!");
-    }
-    else if(humanChoice === "scissors" && computerChoice === "rock"){
-        computer +=1;
-        console.log("Rock beats Scissors, You lose");
-    }
-    else if(humanChoice === "rock" && computerChoice === "paper"){
-        computer +=1;
-        console.log("Paper beats Rock, You lose");
-    }
-    else{
-        computer +=1;
-        console.log("Scissors beat paper, You lose");
-    };
+
+    if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        human++;
+        console.log(`${humanChoice} beats ${computerChoice}, You win!`);
+    } else {
+        computer++;
+        console.log(`${computerChoice} beats ${humanChoice}, You lose!`);
+    }    
 }
 
-function playGame(){
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    console.log("Computer chose " + computerSelection);
-    console.log(human);
-    console.log(computer);
-}
-//For playing game 5 times
-playGame()
-playGame()
-playGame()
-playGame()
-playGame()
+rock.addEventListener("click", () => {playRound("rock",getComputerChoice())});
+paper.addEventListener("click", () => {playRound("paper",getComputerChoice())});
+scissors.addEventListener("click", () => {playRound("scissors",getComputerChoice())})
